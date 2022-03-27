788dd08429fa26b62327299c5a0e41513a0cecb9
Ryota Mizumaki <statham.programming@gmail.com>
2022-03-27 22:04:11 +0900

    Add 404

--- website/root/src/pages/404.tsx
+++ website/root/src/pages/404.tsx
@@ -1,5 +1,20 @@
-const NotFoundPage: React.VFC = () => {
-  return <h1>404 - Page Not Found</h1>;
+import { DashboardLayout } from '~/components/templates/layouts/DashboardLayout';
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ margin: '1em' }}>
+      <h2>404 - Page Not Found</h2>
+      <p>ページが見つかりませんでした。</p>
+    </div>
+  );
+};
+
+NotFoundPage.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <DashboardLayout backgroundColor='red' color='#fff'>
+      {page}
+    </DashboardLayout>
+  );
 };
 
 export default NotFoundPage;
