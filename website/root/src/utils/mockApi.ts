0f2d047756d8c951f4eaeed87920e878753c7dc2
Ryota Mizumaki <statham.programming@gmail.com>
2022-04-03 12:35:23 +0900

    remove unused

--- website/root/src/utils/mockApi.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Debug code for mocking API
- */
-export const mockApi = <T>(res: T, options?: { timeout?: number }): Promise<T> => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(res), options?.timeout ?? 100);
-  });
-};
